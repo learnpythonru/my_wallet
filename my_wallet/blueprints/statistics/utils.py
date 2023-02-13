fbfad3093bf576c547ed302f0a409a84be7d13bc
black <no_email@email.com>
2023-02-13 22:42:51 +0400

    add asserts

--- my_wallet/blueprints/statistics/utils.py
+++ my_wallet/blueprints/statistics/utils.py
@@ -3,3 +3,11 @@ import datetime
 
 def is_report_range_valid(date_from: datetime.date, date_to: datetime.date) -> bool:
     return date_to <= date_from
+
+
+if __name__ == '__main__':
+
+    test_date_from = datetime.datetime.strptime('13/02/23 13:55:26', '%d/%m/%y %H:%M:%S')
+    test_date_to = datetime.datetime.strptime('13/01/23 13:55:26', '%d/%m/%y %H:%M:%S')
+    assert is_report_range_valid(date_from = test_date_from, date_to = test_date_to) == True
+
--- my_wallet/utils/config.py
+++ my_wallet/utils/config.py
@@ -6,3 +6,16 @@ def get_connection_dsn(config: Mapping[str, Any]) -> str:
         f"postgresql://{config['POSTGRES_USER']}:{config['POSTGRES_PASSWORD']}@"
         f"{config['POSTGRES_HOST']}:{config['POSTGRES_PORT']}/{config['POSTGRES_DBNAME']}"
     )
+
+if __name__ == '__main__':
+    config = {
+        'POSTGRES_USER':'POSTGRES_USER',
+        'POSTGRES_PASSWORD':'POSTGRES_PASSWORD',
+        'POSTGRES_HOST':'POSTGRES_HOST',
+        'POSTGRES_PORT':'POSTGRES_PORT',
+        'POSTGRES_DBNAME':'DATABASE'
+    }
+
+    result_dsn = get_connection_dsn(config)
+    print(result_dsn)
+    assert result_dsn == 'postgresql://POSTGRES_USER:POSTGRES_PASSWORD@POSTGRES_HOST:POSTGRES_PORT/DATABASE'
