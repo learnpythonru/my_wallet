8b73b5f751a8bfbb4c9644d96f14ac766d1e9a4a
NikitaTiv <118524352+NikitaTiv@users.noreply.github.com>
2023-02-15 12:06:53 +0300

    написал автотесты для функций is_report_range_valid,get_connection_dsn

--- my_wallet/blueprints/statistics/utils.py
+++ my_wallet/blueprints/statistics/utils.py
@@ -1,5 +1,20 @@
-import datetime
+from datetime import datetime
 
 
 def is_report_range_valid(date_from: datetime.date, date_to: datetime.date) -> bool:
     return date_to <= date_from
+
+
+if __name__ == '__main__':
+    date_to = '2001-02-15 11:47:37.088072'
+    date_from = '2023-03-18 20:15:35.088006'
+    assert is_report_range_valid(date_from, date_to) == (date_to <= date_from)
+
+    date_to = '2023-03-18 20:15:35.088006'
+    date_from = '2023-03-18 20:15:35.088006'
+    assert is_report_range_valid(date_from, date_to) == (date_to <= date_from)
+
+    date_to = '2051-02-15 11:47:37.088072'
+    date_from = '2023-03-18 20:15:35.088006'
+    assert is_report_range_valid(date_from, date_to) == (date_to <= date_from)
+    
